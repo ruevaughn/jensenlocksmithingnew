8890184bbf177ca1778fca5b365d13369b57cf7e
Chase Jensen <soccerchase@gmail.com>
2016-07-10 17:19:46 -0600

    Add links and pages to all routes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,11 @@
 //= require_tree .
 //= require bootstrap-sprockets
 
+$('.navbar li').click(function(e) {
+    $('.navbar li.active').removeClass('active');
+    var $this = $(this);
+    if (!$this.hasClass('active')) {
+        $this.addClass('active');
+    }
+    e.preventDefault();
+});
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,18 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
+  get "automotive", to: "pages#automotive", as: "automotive"
+  get "automotive_hurricane", to: "pages#automotive_hurricane", as: "automotive_hurricane"
+  get "automotive_springdale", to: "pages#automotive_springdale", as: "automotive_springdale"
+  get "automotive_st_george", to: "pages#automotive_st_george", as: "automotive_st_george"
+  get "automotive_kanab", to: "pages#automotive_kanab", as: "automotive_kanab"
+
+  get "blog", to: "pages#blog", as: "blog"
+  get "commercial", to: "pages#commercial", as: "commercial"
+  get "contact", to: "pages#contact", as: "contact"
+  get "home", to: "pages#home", as: "home"
+  get "residential", to: "pages#residential", as: "residential"
+  get "reviews", to: "pages#reviews", as: "reviews"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
