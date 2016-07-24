24a9008d2b5aaa48f738546e81cf3ab2f130cdc1
Chase Jensen <soccerchase@gmail.com>
2016-07-24 11:41:49 -0600

    Wire up creating blog post

--- app/controllers/admin/blog_posts_controller.rb
+++ app/controllers/admin/blog_posts_controller.rb
@@ -9,4 +9,20 @@ class Admin::BlogPostsController < ApplicationController
     @blog_post = BlogPost.new
   end
 
+  def create
+    @blog_post = BlogPost.create(blog_post_params)
+    if @blog_post.valid?
+      flash[:notice] = "Blog Post successfully created."
+      redirect_to admin_blog_posts_path
+    else
+    end
+
+  end
+
+  private
+
+  def blog_post_params
+    params.require(:blog_post).permit(:header, :body)
+  end
+
 end
