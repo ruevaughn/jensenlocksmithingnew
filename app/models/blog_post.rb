1c613efbe2910b6a352a41219763da82785dfe06
Chase Jensen <soccerchase@gmail.com>
2017-01-23 00:26:37 -0700

    Annotations ran

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: blog_posts
+#
+#  id         :integer          not null, primary key
+#  header     :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class BlogPost < ApplicationRecord
 end
