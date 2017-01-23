3f48a9cfefd2996330cfe379f16e4784f927c745
Chase Jensen <soccerchase@gmail.com>
2017-01-23 00:49:01 -0700

    Style form and add update and edit actions

--- app/controllers/admin/page_contents_controller.rb
+++ app/controllers/admin/page_contents_controller.rb
@@ -5,7 +5,25 @@ class Admin::PageContentsController < ApplicationController
   end
 
   def update
+    @page_content = PageContent.find(params[:id])
+
+    if @page_content.update_attributes(page_content_params)
+      flash[:notice] = "#{@page_content.page} - #{@page_content.content_name} updated Successfully"
+      # TODO: Fix this and decide where to redirect
+      # redirect_to "admin_#{@page_content.page}_path"
+
+      redirect_to "/admin/#{@page_content.page}"
+    else
+      flash[:notice] = "There was a problem, contact Chase :)" 
+      render @page_content
+    end
+    # TODO: Update, re-render etc.
   end
+
+  def page_content_params
+    params.require(:page_content).permit(:body)
+  end
+
 
 
 end
