57bf5d356da234bf416124c0467847e9235addf2
Chase Jensen <soccerchase@gmail.com>
2017-01-23 00:30:28 -0700

    Seed data for page_contents

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,16 @@
 
 Admin.delete_all
 Admin.create(email: 'soccerchase@gmail.com', password: 'password', password_confirmation: 'password')
+
+# t.string :page
+# t.string :content_name
+# t.text :body
+
+PageContent.delete_all
+PageContent.create({
+  page: 'automotive',
+  content_name: 'Main Text',
+  body: "Have you locked your keys in your car? Have you lost your keys? No need to worry, put a smile on your face. At Jensen Locksmithing we help people out with automotive lock and/or key problems. Whether it’s unlocking your car or replacing lost or broken keys and remotes, we've got you covered at an affordable rate.\n
+         We want to help if anything like this occurs. With technology on the rise today, automotive technology is increasing as well.\n
+         We stay up to date with this automotive technology, and we can help you out with our state of the art equipment. Avoid towing costs and high dealership prices.\n"
+})
