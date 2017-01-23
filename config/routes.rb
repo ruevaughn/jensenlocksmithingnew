4c75b07d085b2c6c42f5bac53d2402d947e84c02
Chase Jensen <soccerchase@gmail.com>
2017-01-23 00:29:28 -0700

    Routes for admin page_contents and static admin automotive

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,9 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :blog_posts
+    resources :page_contents
+
+    get "automotive", to: "pages#automotive"
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
