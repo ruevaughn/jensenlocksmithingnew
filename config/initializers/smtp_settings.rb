1a26db607df833813a9693637079faa57fa3e6a0
Chase Jensen <soccerchase@gmail.com>
2017-02-02 00:15:01 -0700

    Fix failing build

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => "gmail.com",
-    :user_name => secrets[:GMAIL_USERNAME],
-    :password => secrets[:GMAIL_PASSWORD], 
+    :user_name => ENV['GMAIL_USERNAME'],
+    :password => ENV['GMAIL_PASSWORD'], 
     :authentication => "plain",
     :enable_starttls_auto => true
 }
