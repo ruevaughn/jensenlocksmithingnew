35e09668d60cdcd365edf6b40988c2b2e8786720
Chase Jensen <soccerchase@gmail.com>
2017-02-02 00:17:10 -0700

    Move to production smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,16 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            =>  secrets[:GMAIL_USERNAME],
+      :password             =>  secrets[:GMAIL_PASSWORD],
+      :authentication       => :plain,
+      :enable_starttls_auto => true
+ }
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
